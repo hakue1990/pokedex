a1ddecb905ebc234e19c61d2a0049868bab2cde9
Adam Hałdaś <adam.haldas@gmail.com>
2020-09-10 11:23:13 +0200

    Updates

---
+++ precache-manifest.c3118252ebbd401d4e9a907568358be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9705a786372f9ad1be0b4cace7f924b6",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "255d58c0b9610cefb581",
+    "url": "/pokedex/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ecb1a1fed1772b81a331",
+    "url": "/pokedex/static/js/2.422c94c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.422c94c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255d58c0b9610cefb581",
+    "url": "/pokedex/static/js/main.faa6b265.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
